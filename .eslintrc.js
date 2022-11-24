c4ac1bc151efd68a8901ec0bd4467f012053b6c8
vijay chouhan <vijaychouhan@192.168.0.114>
2022-11-24 11:59:19 +0530

    React Js boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    Node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    "semi": "off",
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>React Js boilerplate !</div>;
+};
+
+export default App;
